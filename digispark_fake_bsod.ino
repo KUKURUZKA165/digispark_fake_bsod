0341e575bbf1cd3f12461df0ced23f59033676ad
KUKURUZKA <60149954+KUKURUZKA165@users.noreply.github.com>
2021-05-30 10:24:37 +0300

    Create digispark_fake_bsod.ino

---
+++ digispark_fake_bsod.ino
@@ -0,0 +1,34 @@
+#include "DigiKeyboard.h"
+#define KEY_UP_ARROW     0x52
+#define KEY_DOWN_ARROW   0x51
+#define KEY_LEFT_ARROW   0x50
+#define KEY_RIGHT_ARROW  0x4F
+#define KEY_LEFT_GUI     0xE3
+#define KEY_ESC          0x29
+#define KEY_HOME         0x4A
+#define KEY_INSERT       0x49
+#define KEY_NUM_LOCK     0x53
+#define KEY_SCROLL_LOCK  0x47
+#define KEY_CAPS_LOCK    0x39
+#define KEY_TAB          0x2B
+
+void digiBegin() {
+  DigiKeyboard.sendKeyStroke(0,0);
+  DigiKeyboard.delay(50);
+}
+
+void printText(fstr_t *txt) {
+  DigiKeyboard.print(txt);
+  DigiKeyboard.update();
+}
+
+void setup() {
+  digiBegin();
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(100);
+  printText(F("iexplore -k https://fakeupdate.net/win10ue/bsod.html"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+}
+
+/* Unused endless loop */
+void loop() {}
